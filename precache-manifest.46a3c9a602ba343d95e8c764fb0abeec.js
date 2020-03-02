b07a3b837170e8a073079d92ff75752cc3ac9490
silvioaburto <sjortiz@ucdavis.edu>
2020-03-01 21:35:00 -0800

    Updates

---
+++ precache-manifest.46a3c9a602ba343d95e8c764fb0abeec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31508b17de08c93e28420f2537b4ba0",
+    "url": "/memory_game/index.html"
+  },
+  {
+    "revision": "067eebefd0ea643773d9",
+    "url": "/memory_game/static/css/main.ee9f67cb.chunk.css"
+  },
+  {
+    "revision": "be2d227bbacb09f60b11",
+    "url": "/memory_game/static/js/2.2688e096.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory_game/static/js/2.2688e096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067eebefd0ea643773d9",
+    "url": "/memory_game/static/js/main.8f67cc39.chunk.js"
+  },
+  {
+    "revision": "36b2bc2b4bebd273bde5",
+    "url": "/memory_game/static/js/runtime-main.afcf22b2.js"
+  }
+]);
